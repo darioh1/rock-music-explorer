e2d14d15e794823d9bed2b2a6e7d08fda59d9798
Dario Herceg <dario.herceg@fer.hr>
2026-09-16 19:02:19 +0200

    Set up initial Java Maven project

---
+++ src/main/java/hr/dario/rockmusic/Main.java
@@ -0,0 +1,11 @@
+package hr.dario.rockmusic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("       ROCK MUSIC EXPLORER");
+        System.out.println("=================================");
+        System.out.println();
+        System.out.println("Enter artist name: ");
+    }
+}
